c251b024e5f60eb21d37760461796b39faca3c84
Muthukrishnan Gurusamy <muthukrishnan.gurusamy@aa.com>
2025-07-16 14:07:42 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,9 @@ async function run(): Promise<void> {
 
     const octokit: InstanceType<typeof GitHub> = github.getOctokit(github_token)   
     
-    console.log('sender:' sender_login)
+    // @ts-ignore
+    // @ts-ignore
+    console.log('sender: ', sender_login);
 
     // if the sender in the azure-boards bot or dependabot, then exit code
     // nothing needs to be done
