0d4a64dcf0b73a79219febba64184855e2f702e9
Muthukrishnan Gurusamy <muthukrishnan.gurusamy@aa.com>
2025-07-16 14:04:32 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -19,11 +19,11 @@ async function run(): Promise<void> {
 
     const octokit: InstanceType<typeof GitHub> = github.getOctokit(github_token)   
     
-    console.log(sender_login)
+    console.log('sender:' sender_login)
 
     // if the sender in the azure-boards bot or dependabot, then exit code
     // nothing needs to be done
-    console.log('Action started. Sender:', sender_login);
+    console.log('Action started. Sender:', sender_login)
 
     if (sender_login === "dependabot[bot]") {
       console.log(`dependabot[bot] sender, exiting action.`)
